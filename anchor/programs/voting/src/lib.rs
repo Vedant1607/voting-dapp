9fde110b741b44c73edfed271d97619910bafd7f
Vedant Sinha <sinhavedant0705@gmail.com>
2026-03-25 16:58:44 +0000

    Initialize a voting dapp

---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
+
+#[cfg(test)]
+mod tests;
+
+declare_id!("411p2YeAVu4pQQkP25Nhu6G3gNBjQ1Bs8A5V3g6ujmsf");
+
+#[program]
+pub mod voting {
+    
+}
